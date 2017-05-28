672272032d35a106c80a7b18351b754444f84b7e
Harold <hvalenzuela@gmail.com>
2017-05-28 13:13:22 -0700

    Update createtable.php
    Using mysqli to make the connection for use on newer PHP 7.

--- setup/createtable.php
+++ setup/createtable.php
@@ -2,59 +2,68 @@
 
 // ***********************************************
 // Standard MySQL Connection Strings
+// ***********************************************
 
-// DB connection info
+//*** DB connection info
 $host = 'dbhost';
 $user = 'dbusername';
 $pwd = 'dbusernamepassword';
 $db = 'database';
+$port = '3306';
+
+//*** Connect using mysqli
+//$conn = new mysqli($host, $user, $pwd, $db, $port);  //*** For using $port
+$conn = new mysqli($host, $user, $pwd, $db);
+//*** Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 // ***********************************************
 // End of Standard MySQL database connectivity
-
 // ***********************************************
-// Azure MySQL Connection Strings
-// Uncomment the sections below to use on Azure websites with MySQL In App
-// Make sure to comment out the Standard MySQL Connection Strings above
 
-// $connectstr_dbhost = '';
-// $connectstr_dbname = '';
-// $connectstr_dbusername = '';
-// $connectstr_dbpassword = '';
+//*** Azure MySQL Connection Strings
+//*** Uncomment the sections below to use on Azure websites with MySQL In App
+//*** Make sure to comment out the Standard MySQL Connection Strings above
 
-// foreach ($_SERVER as $key => $value) {
-//     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-//       continue;
-//     }
-    
-//     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-//     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-//     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-//     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-// }
+//$AzureConnString = $_SERVER['MYSQLCONNSTR_localdb'];
+//$AzureConnStringPieces = explode(";", $AzureConnString);
+//foreach ($AzureConnStringPieces as $piece) {
+    //*** Parse each piece of the string creating $Data_Source (Server:Port), $User_Id (Username), $Password, $Database (DBName)
+    //*** IE. Data Source=127.0.0.1:45678 --> $Data_Source = "127.0.0.1:45678"
+//    parse_str($piece);
+//}
 
-// DB connection info
-// $host = $connectstr_dbhost;
-// $user = $connectstr_dbusername;
-// $pwd = $connectstr_dbpassword;
-// $db = $connectstr_dbname;
+//*** Separate host from port in Data_Source
+//$source = explode(":", $Data_Source);
+//$Host = $source[0];
+//$Port = $source[1];
+
+//*** Create connection
+//$conn = new mysqli($Host, $User_Id, $Password, $Database, $Port);
+//if ($conn->connect_error){
+//    die("Connection failed: " . $conn->connect_error);
+//} 
 // ***********************************************
 // End of Azure MySQL In App database connectivity
+// ***********************************************
 
-try{
-    $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE graffiti_tbl(
-                id INT NOT NULL AUTO_INCREMENT, 
-                PRIMARY KEY(id),
-                tagger VARCHAR(30),
-                message VARCHAR(128),
-				location VARCHAR (16),
-                date DATE)";
-    $conn->query($sql);
+//*** sql to create table
+$sql = "CREATE TABLE graffiti_tbl(
+        id INT NOT NULL AUTO_INCREMENT, 
+        PRIMARY KEY(id),
+        tagger VARCHAR(30),
+        message VARCHAR(128),
+        location VARCHAR (16),
+        date DATE)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "<h3>Table created.</h3>";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
+
+//*** Close mysqli connection
+$conn->close();
+
 ?>
- 
